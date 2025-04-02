510deb945cffea5dd7e336241f74b3dbdb56259f
Uliana-Pylypenko <upylypen@visa.com>
2025-04-02 15:41:45 +0200

    user registration first attempt

--- src/main/java/pl/coderslab/finalproject/controller/UserController.java
+++ src/main/java/pl/coderslab/finalproject/controller/UserController.java
@@ -3,13 +3,15 @@ package pl.coderslab.finalproject.controller;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import pl.coderslab.finalproject.dto.UserDTO;
 import pl.coderslab.finalproject.service.UserService;
 
 import java.util.List;
 
-@RestController
+@Controller // thymeleaf doesn't work well with RestController
 @RequestMapping("/user")
 @AllArgsConstructor
 public class UserController {
@@ -29,6 +31,31 @@ public class UserController {
     public ResponseEntity<String> create(@RequestBody UserDTO userDTO) {
         return userService.create(userDTO);
     }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        return "registration";
+    }
+
+    @PostMapping("/register")
+    public String register(@RequestParam String username,
+                           @RequestParam String email,
+                           @RequestParam String password,
+                           Model model) {
+        ResponseEntity<String> response = userService.register(username, email, password);
+        if (response.getStatusCode().is2xxSuccessful()) {
+            return "redirect:/";
+        } else {
+            model.addAttribute("error", response.getBody());
+            return "registration";
+        }
+    }
+
+    @GetMapping("/success")
+    public String showSuccessPage() {
+        return "success";
+    }
+
 
     @PutMapping("/update/{id}")
     public ResponseEntity<String> update(@PathVariable Long id, @RequestBody UserDTO userDTO) {
